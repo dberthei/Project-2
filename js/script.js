10a2555f8d390d98cb09d6f9b755e6682be904fd
ythonippara <47587277+ythonippara@users.noreply.github.com>
2020-04-28 03:19:24 -0500

    Added new JS code for a slideshow

--- js/script.js
+++ js/script.js
@@ -80,25 +80,36 @@ function init() {
   button.addEventListener('click', Alert);
 }
 window.addEventListener('load', init);
-  
-  /* Slider for index.html */
+
+/*
+Slider for index.html
+*/
 function init() {
   var slideIndex = 1;
-  showImage(slideIndex);
+  showSlides(slideIndex);
   
-  function plusImage(n) {
-    showImage(slideIndex += n);
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
   }
   
-  function showImage(n) {
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
     var i;
-    var x = document.getElementsByClassName("image_container");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+    var slides = document.getElementsByClassName("slide_container");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-    x[slideIndex-1].style.display = "block";
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
   }
 }
 window.addEventListener('load', init);
